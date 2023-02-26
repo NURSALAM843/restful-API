75c5fca5e1efc10334378159025e369456262ae2
NURASALAM843 <asgardasgarin12@gmail.com>
2023-02-26 19:01:03 +0800

    perbaikan login

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="mb-3">
+        <label for="formGroupExampleInput" class="form-label">{{ $label }}</label>
+        <input type="{{ $type }}" class="form-control" id="formGroupExampleInput"
+            placeholder="{{ $label }}">
+    </div>
+</div>
